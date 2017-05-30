6ba77b2d5aaa31fcbd3ce3f679a2dfa2b0f52c01
Luiz Ribeiro <luizribeiro@gmail.com>
2017-05-29 22:05:08 -0400

    Testing prettier + lint-staged

--- hello/static/js/src/Hello.js
+++ hello/static/js/src/Hello.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import {Button} from 'react-bootstrap';
+import React from "react";
+import { Button } from "react-bootstrap";
 
 export default function Hello() {
-  return (
-    <Button>Hello</Button>
-  );
+  return <Button>Hello</Button>;
 }
