941d33a265a7a7f43dff872623a9b8a78bc6985f
Luiz Ribeiro <luizribeiro@gmail.com>
2017-09-03 16:25:53 -0400

    Make /graphql/ CSRF exempt for now

--- sandbox/urls.py
+++ sandbox/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
 import hello.views
@@ -6,6 +7,7 @@ import hello.views
 urlpatterns = [
     url(r'^$', hello.views.nest, name='nest'),
     url(r'^react$', hello.views.react, name='react'),
+    url(r'^graphql/$', csrf_exempt(GraphQLView.as_view(graphiql=False)), name='graphql'),
     url(r'^graphiql/$', GraphQLView.as_view(graphiql=True), name='graphiql'),
 ]
 
