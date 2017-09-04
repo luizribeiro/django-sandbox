2c41d7fbfad6ea99f310b38416743ddbccb04a22
Luiz Ribeiro <luizribeiro@gmail.com>
2017-09-04 10:46:22 -0400

    Added tests for GraphQL query

--- hello/tests.py
+++ hello/tests.py
@@ -1,7 +1,35 @@
-from django.test import TestCase
+from django.test import (
+    Client,
+    TestCase,
+)
+import json
+from unittest.mock import (
+    patch,
+    MagicMock,
+)
 
 
 class HelloTest(TestCase):
-    def test_nothing(self) -> None:
-        pass
+    def test_graphql_response(self) -> None:
+        nest_mock = MagicMock(
+            thermostats=[
+                MagicMock(
+                    mode='off',
+                    current_temperature=24,
+                    target_temperature=23.5,
+                ),
+            ],
+        )
+
+        with patch('hello.schema.Nest', return_value=nest_mock):
+            response = Client().get(
+                '/graphql/',
+                {'query': '{thermostat{mode},vacuum{state}}'},
+            )
+            assert json.loads(response.content.decode('utf-8')) == {
+                'data': {
+                    'thermostat': {'mode': 'OFF'},
+                    'vacuum': {'state': 'CHARGING'},
+                },
+            }
 
