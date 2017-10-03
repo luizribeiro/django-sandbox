0fba7976d89dde16586fb8bcc768baba22e6a768
Luiz Ribeiro <luizribeiro@gmail.com>
2017-10-03 03:15:52 -0300

    Removed home.views which wasnt used

--- conf/urls.py
+++ conf/urls.py
@@ -5,13 +5,10 @@ from django.conf.urls import (
 )
 
 import backend.views
-import home.views
 
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^$', home.views.nest, name='nest'),
-    url(r'^react$', home.views.react, name='react'),
     url(r'^graphql/$', backend.views.graphql, name='graphql'),
     url(r'^graphiql/$', backend.views.graphiql, name='graphiql'),
 
