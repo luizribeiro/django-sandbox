59312179d8b39f36d421330eb3f1c8e36b7d2fdf
Luiz Ribeiro <luizribeiro@gmail.com>
2018-06-17 11:55:37 -0400

    Downgraded django-apscheduler to 0.2.9 due to jarekwg/django-apscheduler#25

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,5 +2,4 @@ from apscheduler.jobstores.memory import MemoryJobStore
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 scheduler = AsyncIOScheduler()
-scheduler.add_jobstore(MemoryJobStore(), 'default')
 
