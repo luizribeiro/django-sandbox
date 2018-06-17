9b9e48a6f6d324f9759af6d7b8bd698779880964
Luiz Ribeiro <luizribeiro@gmail.com>
2018-06-17 11:41:42 -0400

    Use MemoryJobStore for apscheduler

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,4 +1,6 @@
+from apscheduler.jobstores.memory import MemoryJobStore
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 scheduler = AsyncIOScheduler()
+scheduler.add_jobstore(MemoryJobStore(), 'default')
 
